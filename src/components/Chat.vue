a67c72ac5d0543ee9c150b607c8e6177e8637c7a
Yohan Le Bricquer <ylebricq>
2019-10-08 17:48:34 +0200

    Initialisation of the first components with their routes : Index Chat PageNotFound

---
+++ src/components/Chat.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+
+        <p> CHAT </p>
+
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'chat',
+
+  data: () => ({
+    url: 'http://localhost:4000' // 'http://localhost:4000'
+  })
+}
+</script>
