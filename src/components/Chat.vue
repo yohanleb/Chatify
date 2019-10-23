051726bacd7146832dcc93c3fb7df0b7639ebbbf
Doremi <remi.truong98@gmail.com>
2019-10-23 14:32:20 +0200

    Improve chat vue + correction
    Amélioration de la liste des users connectés au chat
    Amélioration de la zone de texte
    Ajout du bouton envoyer message

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,24 +1,20 @@
 <template>
   <v-layout row>
-    <v-flex>
-  <v-app-bar
-      app
-      clipped-right
-      color="blue-grey"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>{{ chatName }}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn color="red darken-2" dark right="true" @click="logout()">
-        <v-icon dark left>mdi-exit-to-app</v-icon>
-        Logout
-      </v-btn>
-  </v-app-bar>
+    <v-flex style="position: relative;">
+      <v-app-bar app clipped-right color="blue-grey" dark>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>{{ chatName }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn color="red darken-1" dark @click="logout()">
+          <v-icon dark left>exit_to_app</v-icon>Logout
+        </v-btn>
+      </v-app-bar>
 
       <v-navigation-drawer v-model="drawer" app>
-        <v-list dense disabled>
-          <v-list-item @click.stop="left = !left" v-for="(user, i) in users" :key="i">
+        <v-list>
+          <v-list-item v-for="(user, i) in users" :key="i">
+            <v-icon>account_circle</v-icon>
+            <v-spacer></v-spacer>
             <v-list-item-content>
               <v-list-item-title>{{ user.name }}</v-list-item-title>
             </v-list-item-content>
@@ -26,11 +22,10 @@
         </v-list>
       </v-navigation-drawer>
 
-      <v-navigation-drawer v-model="left" fixed temporary></v-navigation-drawer>
+      <v-navigation-drawer fixed temporary></v-navigation-drawer>
 
       <div class="chat-container">
-        <v-list disabled>
-          <v-list-item-group>
+        <v-list>
             <!-- La liste des messages -->
             <v-list-item
               class="message"
@@ -49,12 +44,13 @@
                 </v-tooltip>
               </v-list-item-content>
             </v-list-item>
-          </v-list-item-group>
         </v-list>
       </div>
-
-      <div class="typeBox">
-        <input type="text" placeholder="Write a message" v-model="content" v-on:keyup.enter="sendMessage()"/>
+      <div class="typebox">
+        <input type="text" placeholder="Ecrire un message..." v-on:keyup.enter="sendMessage" v-model="content">
+        <v-btn class="btnMsg" color="blue darken-3" dark @click="sendMessage()">
+          <v-icon dark left>message</v-icon>Send
+        </v-btn>
       </div>
     </v-flex>
   </v-layout>
@@ -65,8 +61,6 @@ export default {
   data () {
     return {
       drawer: null,
-      left: false,
-      right: false,
       errorMessage: '',
       messages: this.getMessages(),
       users: this.getUsers(),
@@ -103,12 +97,9 @@ export default {
     },
     async getUsers () {
       try {
-        const response = await this.axios.post(
-          this.$apiURL + '/api/users/',
-          {
-            chatID: this.chatID
-          }
-        )
+        const response = await this.axios.post(this.$apiURL + '/api/users/', {
+          chatID: this.chatID
+        })
         if (response.data.error === 0) {
           this.users = response.data.users
         } else {
@@ -158,29 +149,30 @@ export default {
 </script>
 
 <style>
-.typeBox {
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  bottom: 0;
-  height: 4.9rem;
-  width: 100%;
-  background-color: #fff;
-}
-.typeBox input[type="text"] {
-  position: absolute;
-  left: 1.5rem;
-  padding: 1rem;
-  background-color: transparent;
-  outline: none;
-  font-size: 1.25rem;
-}
+  .typebox{
+    
+    position: relative;
+    border-top: 0.25rem solid #607d8b;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    height: 5rem;
+  }
+
+  .typebox input[type=text]{
+    position: relative;
+    height: 3.5rem;
+    left: 2.5rem;
+    width: 75rem;
+    outline: none;
+    font-size: 1.25rem;
+  }
 .chat-container {
-  height: calc(100vh - 9.5rem);
+  margin: 1rem;
   overflow-y: auto;
   background-color: #f2f2f2;
 }
-.message.user{
+.message.user {
   padding-right: 50%;
 }
 .message.own {
@@ -193,6 +185,9 @@ export default {
 .chat-container .username {
   font-size: 18px;
   font-weight: bold;
+}
+.btnMsg {
+  margin-right: 1rem;
 }
 .chat-container .content {
   background-color: lightgreen;
